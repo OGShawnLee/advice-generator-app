055ce43f47e30cb3f9a8afd113909da6159b978e
Shawn Lee <changoloco.evg@gmail.com>
2022-04-04 10:19:06 -0500

    :recycle: refactor: make global and move button animation className to layout

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -39,11 +39,3 @@
 		</form>
 	</div>
 </main>
-
-<style>
-	button:hover {
-		--tw-shadow-color: hsl(150, 100%, 66%, 50%);
-		box-shadow: 0px 0px 30px 5px var(--tw-shadow-color);
-		@apply hover:(scale-110 duration-150 rotate-180);
-	}
-</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,3 +3,11 @@
 </script>
 
 <slot />
+
+<style>
+	:global(button:hover) {
+		--tw-shadow-color: hsl(150, 100%, 66%, 50%);
+		box-shadow: 0px 0px 30px 5px var(--tw-shadow-color);
+		@apply hover:(scale-110 duration-150 rotate-180);
+	}
+</style>
